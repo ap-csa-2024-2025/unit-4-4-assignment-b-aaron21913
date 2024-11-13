7bfe95d2f89051a7e4e88d117124a4b008cafd04
aaron cheese skibidi rizz <126525235+aaron21913@users.noreply.github.com>
2024-11-13 19:04:58 +0000

    rizz

--- Main.java
+++ Main.java
@@ -9,12 +9,24 @@ public class Main
 
   public static int countSub(String word, String target)
   {
-    return 0;
+    String sentence = "gagagigo the giga risen";
+    String target = "ga";
+    int count = 0;
+
+    for (int i = 0; i < sentence.length()-5; i++)
+    {
+      String L = sentence.substring(i, i+6);
+      if (L.equals(target))
+      {
+        count++;
+      }
+    }
+    return count;
   }
 
   public static int countProperContains(String word, String target)
   {
-    return 0;
+    
   }
 
   public static String deleteSub(String word, String target)
