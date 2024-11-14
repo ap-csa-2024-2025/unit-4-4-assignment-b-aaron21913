99eee41b80c69a8702ccb78604974d7b0fd05ac5
aaron cheese skibidi rizz <126525235+aaron21913@users.noreply.github.com>
2024-11-14 19:18:30 +0000

    rizz2

--- Main.java
+++ Main.java
@@ -26,7 +26,51 @@ public class Main
 
   public static int countProperContains(String word, String target)
   {
-    
+    int count = 0;
+    int N = target.length();
+    for (int i = 0; i < word.length() - (N-1); i++)
+    {
+      String sub = word.substring(i, i+N);
+
+      boolean beginOrSpaceBefore;
+      int beginOfTarget = i
+      beginOrSpaceBefore = (beginOfTarget == 0) || word1.substring(beginOfTarget, beginOfTarget+1).equals(" ")
+      if (beginOfTarget == 0)
+      {
+        beginOrSpaceBefore = true;
+      }
+      else
+      {
+        beginOrSpaceBefore = false;
+      }
+      boolean endOrSpaceAfter;
+      int endOfTarget = i + target.length();
+      endOrSpaceAfter = (endOfTarget == word.length()) || word.substring(endOfTarget1, endOfTarget+1).equals(" ")
+      // i is the number index and target.length jsut gets the length of the target string
+      if (endOfTarget == word.length())
+      {
+        endOrSpaceAfter = true;
+      }
+      else
+      {
+        String after = word.substring(endOfTarget, endOfTarget+1);
+        if (after.equals(" "))
+        {
+          endOrSpaceAfter = true;
+        }
+        else{
+          endOrSpaceAfter = false;
+        }
+      }
+
+      boolean properlyContained = beginOrSpaceBefore && endOrSpaceAfter
+      if (sub.equals(target) && properlyContained)
+      {
+        count++
+      }
+    }
+    return count;
+
   }
 
   public static String deleteSub(String word, String target)
